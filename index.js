9198500ffcbb30d56d416efefd127d31d9e06ddf
speedev196 <speedev196@gmail.com>
2017-03-18 07:48:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var port = process.env.PORT || 5000;
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-mongoose.connect('mongodb://');
+mongoose.connect('mongodb://datadata:datadata@ds129010.mlab.com:29010/fakebank');
 
 var Schema= mongoose.Schema;
 
